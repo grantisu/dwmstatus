554eb10a2774b4e513ffb1a416edd88c7fa197e6
Grant Mathews <grant.m.mathews@gmail.com>
2014-12-24 18:43:18 -0800

    Use sysinfo() instead of getloadavg()
    Replace the local getloadavg() call with a sysinfo(&info) call, where
    info is global. This will allow other functions to use the results from
    sysinfo() later.

--- config.h
+++ config.h
@@ -1,19 +1,24 @@
+#include <sys/sysinfo.h>
+
 const char *field_sep = "  \u2022  ";
 const char *time_fmt  = "%a %b %e, %l:%M %p";
 
 const int update_period = 15;
 
+struct sysinfo info;
+
 char *
 loadavg(void)
 {
-	double avgs[3];
-
-	if (getloadavg(avgs, 3) < 0) {
-		perror("getloadavg");
+	if (sysinfo(&info) < 0) {
+		perror("sysinfo");
 		exit(1);
 	}
 
-	return smprintf("%.2f %.2f %.2f", avgs[0], avgs[1], avgs[2]);
+	return smprintf("%.2f %.2f %.2f",
+		info.loads[0] / 65536.0f,
+		info.loads[1] / 65536.0f,
+		info.loads[2] / 65536.0f);
 }
 
 char *
