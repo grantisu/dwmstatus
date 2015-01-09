1b8d20cd34eed26230b7a68c4c87bd45fe284bbd
Grant Mathews <grant.m.mathews@gmail.com>
2015-01-09 09:53:47 -0800

    Revert "Use sysinfo() instead of getloadavg()"
    This reverts commit 94c61fa57ea53fc68f0b896ea2630ca044c0b0fb.
    
    Since sysinfo() is no longer being used for memory stats, there's no
    point in keeping it around.

--- config.h
+++ config.h
@@ -1,24 +1,19 @@
-#include <sys/sysinfo.h>
-
 const char *field_sep = "  \u2022  ";
 const char *time_fmt  = "%a %b %e, %l:%M %p";
 
 const int update_period = 15;
 
-struct sysinfo info;
-
 char *
 loadavg(void)
 {
-	if (sysinfo(&info) < 0) {
-		perror("sysinfo");
+	double avgs[3];
+
+	if (getloadavg(avgs, 3) < 0) {
+		perror("getloadavg");
 		exit(1);
 	}
 
-	return smprintf("%.2f %.2f %.2f",
-		info.loads[0] / 65536.0f,
-		info.loads[1] / 65536.0f,
-		info.loads[2] / 65536.0f);
+	return smprintf("%.2f %.2f %.2f", avgs[0], avgs[1], avgs[2]);
 }
 
 char *
